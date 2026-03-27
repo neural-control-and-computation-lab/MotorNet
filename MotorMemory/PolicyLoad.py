ba1e64d727a71de26dccea88fd756df5b925b512
Poune Mirzazadeh <52932937+MirzazadehPoune@users.noreply.github.com>
2026-03-27 16:57:35 -0400

    Update PolicyLoad.py

--- MotorMemory/PolicyLoad.py
+++ MotorMemory/PolicyLoad.py
@@ -14,8 +14,8 @@ def creat_policy(env, inputs, device, policy_func, optimizer_mod, learning_rate)
     # input sparsity
     vision_dim = np.arange(env.get_vision().shape[1])
     proprio_dim = np.arange(env.get_proprioception().shape[1]) + vision_dim[-1] + 1
-    task_dim = np.arange(inputs['inputs'].shape[2]) + proprio_dim[-1] + 1
-    policy = policy_func(env.observation_space.shape[0] + inputs['inputs'].shape[2], module_sizes, env.n_muscles,
+    task_dim = np.arange(inputs.shape[2]) + proprio_dim[-1] + 1
+    policy = policy_func(env.observation_space.shape[0] + inputs.shape[2], module_sizes, env.n_muscles,
                               vision_dim=vision_dim, proprio_dim=proprio_dim, task_dim=task_dim,
                               vision_mask=vision_mask, proprio_mask=proprio_mask, task_mask=task_mask,
                               connectivity_mask=connectivity_mask, output_mask=output_mask,
