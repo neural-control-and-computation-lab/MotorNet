5f7215352efc63b25a97979f548ffc5a5fb9f6b7
Poune Mirzazadeh <52932937+MirzazadehPoune@users.noreply.github.com>
2026-04-27 22:05:36 -0400

    Update Model.py

--- MotorMemory/Model.py
+++ MotorMemory/Model.py
@@ -23,10 +23,11 @@ def run_episode(env, task, policy, batch_size, n_t, device, force_field = 'null'
         task.cue = 'right'
     elif contextual_cue == 'left':
         task.cue = 'left'
-    elif force_dir == 'CW':
-        task.cue = 'right'
-    elif force_dir == 'CCW':
-        task.cue = 'left'
+    elif contextual_cue == 'force_dependent':
+        if force_dir == 'CW':
+            task.cue = 'right'
+        elif force_dir == 'CCW':
+            task.cue = 'left'
     else:
         task.cue = 'null'
 
@@ -60,11 +61,10 @@ def run_episode(env, task, policy, batch_size, n_t, device, force_field = 'null'
 
         # Compute endpoint load (force field)
         force_mask = (inp[:, t_step, 2].abs() < 1e-3).float().unsqueeze(1)
-        force_field  = applied_load(endpoint_vel = info['states']['cartesian'][:, 2:], k = k, mode = force_dir)
-        masked_force_field = force_field * force_mask
+        applied_force  = applied_load(endpoint_vel = info['states']['cartesian'][:, 2:], k = k, mode = force_dir)
+        masked_force_field = applied_force * force_mask
 
         obs, reward, terminated, truncated, info = env.step(action=action, endpoint_load = masked_force_field)
-        # print(obs, obs.shape)
         xy.append(info['states']['cartesian'][:, None, :])
         all_actions.append(action[:, None, :])
         all_muscle.append(info['states']['muscle'][:, 0, None, :])
@@ -77,6 +77,7 @@ def run_episode(env, task, policy, batch_size, n_t, device, force_field = 'null'
 
     ext_force = force_dir
 
+
     return {
         'xy': th.cat(xy, dim=1),
         'hidden' : th.cat(all_hidden, dim=1),
@@ -87,7 +88,8 @@ def run_episode(env, task, policy, batch_size, n_t, device, force_field = 'null'
         'inp' : th.cat(all_inp, dim=1),
         'vis_inp' : th.cat(all_vis_inp, dim=1),
         'joint' : th.cat(all_joint, dim=1),
-        'ext_force' : ext_force
+        'ext_force' : ext_force,
+        'traj' : task.shift_traj(th.cat(xy, dim=1)),
     }
 
 
@@ -107,7 +109,7 @@ def calculate_loss(task,episode_data):
     total_loss = cartesian_loss + muscle_loss + jerk_loss + spectral_loss
 
     return {
-        'total' : total_loss,
+        'total': total_loss,
         'cartesian': cartesian_loss,
         'muscle': muscle_loss,
         'spectral': spectral_loss,
@@ -137,7 +139,7 @@ def calculate_deviations(task, episode_data, n_t, eps = 1e-6):
 
 
 # Run multiple training batches and optimize the policy.
-def train_batches(env, task, policy, optimizer, n_batches, batch_size, interval, ep_dur, device, run_mode, force_field, contextual_cue, *args, **kwargs ):
+def train_batches(env, task, policy, optimizer, n_batches, batch_size, interval, ep_dur, device, run_mode, force_field, contextual_cue, k, *args, **kwargs ):
 
     total_losses = []
     cartesian_losses = []
@@ -150,7 +152,7 @@ def train_batches(env, task, policy, optimizer, n_batches, batch_size, interval,
     task.run_mode = run_mode
     n_t = int(ep_dur / env.effector.dt) + 1
     for batch in range(n_batches):
-        episode_data = run_episode(env, task, policy, batch_size, n_t, device, force_field, contextual_cue, *args, **kwargs)
+        episode_data = run_episode(env = env, task = task, policy = policy, batch_size = batch_size, n_t = n_t, device = device, force_field = force_field, contextual_cue=contextual_cue, k=k, *args, **kwargs)
         loss_dict = calculate_loss(task, episode_data)
         total_losses.append(loss_dict['total'].item())
         cartesian_losses.append(loss_dict['cartesian'].item())
@@ -167,14 +169,15 @@ def train_batches(env, task, policy, optimizer, n_batches, batch_size, interval,
         endpoint_dev.append(end_dev)
 
         # Save the weights before updating them.
-        if batch % interval == 0 or batch == 1 or batch == n_batches - 1:
+        if batch % interval == 0  or batch == n_batches - 1:
             weight_dic[f'{batch}'] = copy.deepcopy(policy.state_dict())
 
         # backward pass & update weights
+        optimizer.zero_grad()
         loss_dict['total'].backward()
         th.nn.utils.clip_grad_norm_(policy.parameters(),max_norm=1)  # important to make sure gradients don't get crazy
         optimizer.step()
-        optimizer.zero_grad()
+
 
 
     return {'total' : total_losses, 'cartesian': cartesian_losses, 'muscle': muscle_losses, 'spectral': spectral_losses,
@@ -182,13 +185,22 @@ def train_batches(env, task, policy, optimizer, n_batches, batch_size, interval,
 
 
 # Run test episodes without training.
-def test_batches(env , task, policy, n_batches, batch_size, interval, ep_dur, device, run_mode, *args, **kwargs ):
+def test_batches(env , task, policy, n_batches, batch_size, interval, ep_dur, device, run_mode, force_field, contextual_cue, k, *args, **kwargs ):
     task.run_mode = run_mode
     n_t = int(ep_dur / env.effector.dt) + 1
+    endpoint_dev = []
+    lateral_dev = []
     for batch in range(n_batches):
-        episode_data = run_episode(env = env, task = task, policy = policy, batch_size = batch_size, n_t = n_t, device = device, *args, **kwargs)
+        episode_data = run_episode(env=env, task=task, policy=policy, batch_size=batch_size, n_t=n_t, device=device, force_field=force_field, contextual_cue=contextual_cue, k=k, *args, **kwargs)
+        lat_dev, end_dev = calculate_deviations(task, episode_data, n_t)
+        if task.cue == 'left':
+            lat_dev = - lat_dev
+        else:
+            lat_dev = lat_dev
+        lateral_dev.append(lat_dev)
+        endpoint_dev.append(end_dev)
 
-    return episode_data
+    return episode_data, endpoint_dev, lateral_dev
 
 
 
